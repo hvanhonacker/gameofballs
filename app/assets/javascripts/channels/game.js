b125584c7ef45cb4eac8e0fc8dc453b3f7384653
Hugo Van Honacker <hugo@keycoopt.com>
2019-03-06 23:26:22 +0100

    Fix le rendu des evènements (mauvaise game). Ajoute les réactions

--- app/assets/javascripts/channels/game.js
+++ app/assets/javascripts/channels/game.js
@@ -1,29 +1,47 @@
 var subscribeToGameChannel = function(game_id) {
-  App['game' + game_id] = App['game' + game_id] || App.cable.subscriptions.create({ channel: 'GameChannel', game: game_id }, {
+  let game_chanel = 'game_' + game_id
 
-    connected: function() {
-      // Called when the subscription is ready for use on the server
-    },
+  if (App[game_chanel] != null) {
+    console.log("Already subscribed");
+    return App[game_chanel];
+  } else {
+    console.log("Subscribing ...");
 
-    disconnected: function() {
-      // Called when the subscription has been terminated by the server
-    },
+    App[game_chanel] = App.cable.subscriptions.create({ channel: 'GameChannel', game: game_id }, {
+      connected: function() {
+        // Called when the subscription is ready for use on the server
+      },
 
-    received: function(data) {
-      // Called when there's incoming data on the websocket for this channel
-      console.log(data);
-      this.renderEvent(data);
-    },
+      disconnected: function() {
+        // Called when the subscription has been terminated by the server
+      },
 
-    renderEvent: function(data) {
-      $("#events").append(data['content']);
-    }
+      received: function(data) {
+        // Called when there's incoming data on the websocket for this channel
+        this.renderGameEvent(data);
+      },
+
+      renderGameEvent: function(data) {
+        $('.game[data-game-id="' + game_id +'"] #events').append(data['content']);
+      }
+    });
+
+    return App[game_chanel];
+  }
+}
+
+var initSupporterBar = function(channel) {
+  $('.reactor').on('click', function(e) {
+    e.preventDefault();
+    channel.send({ reaction: $(this).text() })
   });
 }
 
 $(document).on('turbolinks:load', function() {
-  let game_id = $('#game').data('game-id')
-  if(game_id != null) {
-    subscribeToGameChannel(game_id);
+  let game_id = $('.game').data('game-id')
+
+  if (game_id != null) {
+    const gameChannel = subscribeToGameChannel(game_id);
+    initSupporterBar(gameChannel);
   }
 });
--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,5 +1,7 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
+    @game = Game.find(params[:game])
+
     stream_from "game_channel:#{params[:game]}"
   end
 
@@ -7,5 +9,10 @@ class GameChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
 
     Rails.logger.debug "unsubscribed"
+  end
+
+  def receive(data)
+    rendered_reaction = ApplicationController.renderer.render(partial: 'game_events/reaction', locals: { reaction: data["reaction"] })
+    ActionCable.server.broadcast "game_channel:#{@game.id}", content: rendered_reaction
   end
 end
