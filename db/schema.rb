3b3e400c439fa28b28b3828f8e395b7f161c18fb
Hugo Van Honacker <hugo@keycoopt.com>
2019-03-09 22:40:03 +0100

    SQlite -> PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_28_140513) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string "player_1_name", null: false
     t.string "player_2_name", null: false
