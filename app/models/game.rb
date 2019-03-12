03d4ab930d4d78622fa619276b522c106cb326b5
Hugo Van Honacker <hugo@keycoopt.com>
2019-03-12 10:17:31 +0100

    Fix responsive

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,13 +4,13 @@ class Game < ApplicationRecord
 
   def self.random_rule
     [
-      "Jamie: Jouez avec une main manquante",
-      "Cersei: Famille - Faire se toucher deux boules de couleurs opposées entraine une pénalité",
-      "Jon: Second souffle - Jouez deux fois de suite",
-      "Hodor: Bloqué - Rentrer une boule entraine une pénalité",
-      "Tyrion: Ivresse - Vous jouez avec votre mauvaise main",
-      "Arya: Camouflage - Vous pouvez toucher les boules adverses en premier sans pénalités",
-      "Theon: Couic - Jouez sans queue",
+      "Jamie : Jouez avec une main manquante",
+      "Cersei : Famille - Faire se toucher deux boules de couleurs opposées entraine une pénalité",
+      "Jon : Second souffle - Jouez deux fois de suite",
+      "Hodor : Bloqué - Rentrer une boule entraine une pénalité",
+      "Tyrion : Ivresse - Vous jouez avec votre mauvaise main",
+      "Arya : Camouflage - Vous pouvez toucher les boules adverses en premier sans pénalités",
+      "Theon : Couic - Jouez sans queue",
       "White Walker : Undead - chaque boule adverse rentrée revient sur la table dans votre couleur",
       "Sansa : Changement de camp - Touchez une boule adverse en premier",
       "Varys : Manipulation - Intervertissez la place de deux boules"
