e56614081bf107b09e3a5e1da4689f55dddc5ea0
Hugo Van Honacker <hugo@keycoopt.com>
2019-03-12 15:10:52 +0100

    Ajout des règles Bran, change Jamie

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,14 +4,15 @@ class Game < ApplicationRecord
 
   def self.random_rule
     [
-      "Jamie : Jouez avec une main manquante",
+      "Jamie : Sans la main ! - Jouez avec un poing fermé",
+      "Bran : Visions - Tirez les yeux fermés",
       "Cersei : Famille - Faire se toucher deux boules de couleurs opposées entraine une pénalité",
       "Jon : Second souffle - Jouez deux fois de suite",
       "Hodor : Bloqué - Rentrer une boule entraine une pénalité",
       "Tyrion : Ivresse - Vous jouez avec votre mauvaise main",
       "Arya : Camouflage - Vous pouvez toucher les boules adverses en premier sans pénalités",
       "Theon : Couic ! - Jouez sans bandes",
-      "White Walker : Undead - chaque boule adverse rentrée revient sur la table dans votre couleur",
+      "White Walker : Undead - Chaque boule adverse rentrée revient sur la table dans votre couleur",
       "Sansa : Changement de camp - Touchez une boule adverse en premier",
       "Varys : Manipulation - Intervertissez la place de deux boules"
     ].sample
